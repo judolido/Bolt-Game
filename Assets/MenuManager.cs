bbf6401eeaffdaf5a4624ab3480cb9aa54712bcd
Viktors Matuls <viktorito10g@inbox.lv>
2024-11-25 01:31:55 +0100

    Player_movement+camera_tracking

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -23,7 +23,8 @@ public class MenuManager : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("World");
+     
+  //      SceneManager.LoadScene("InWorld");
     }
 
     // Function to quit the game
